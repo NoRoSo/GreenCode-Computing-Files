767a16e6e37f322176f32a96899aa331757a5fdc
NoRoSo <69879539+NoRoSo@users.noreply.github.com>
2022-07-19 14:55:10 -0500

    Add files via upload

--- Problem Sets/Flipping Matrix/MatrixGame.cpp
+++ Problem Sets/Flipping Matrix/MatrixGame.cpp
@@ -4,24 +4,27 @@ using namespace std;
 typedef long long ll;
 
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
     ll q;
     cin >> q;
     while(q--) {
         ll n;
-        cin >> n; ll mn = 2*n;
+        cin >> n;
+        ll mn = 2*n; //'n' is the quadrant size, 'mn' is the matrix size.
         ll matrix[mn][mn];
 
-        for (ll i=0;i < mn;i++) {
+        for (ll i=0;i < mn;i++) { //getting input
             for (ll j=0; j < mn; j++)
-		cin >> matrix[i][j];
+                cin >> matrix[i][j];
         }
-        ll maxi = -1; ll sum = 0;
+
+        ll maxi = -1;
+        ll sum = 0;
+
         for (ll i = 0; i < n; i++) {
-            for (ll j = 0; j < n; j++) {
+            for (ll j = 0; j < n; j++) { //iterate through each cell in the quadrant and finds the maximum possibility that it could be and selects that.
                 ll mp = matrix[i][j];
-                mp = max(mp, max(matrix[mn-i-1][j], matrix[i][mn-j-1]));
-                mp = max(mp,matrix[mn-i-1][mn-j-1]);
+                mp = max(mp, max(matrix[mn-i-1][j], matrix[i][mn-j-1])); //compares with the row flip and column flip possibility.
+                mp = max(mp,matrix[mn-i-1][mn-j-1]); //compares with the possibility if you flipped both row and column
                 sum += mp;
             }
         }
--- Problem Sets/Is It a Match/LongestSubsequence.cpp
+++ Problem Sets/Is It a Match/LongestSubsequence.cpp
@@ -5,20 +5,21 @@
 #include <algorithm>
 using namespace std;
 
-void LCS(const vector<int> &A, const vector<int> &B) {
+void LCS(const vector<int> &A, const vector<int> &B) { //Longest Common Subsequence
     int n = A.size(), m = B.size();
-    vector<int> lens(m+1,0);
-    vector<vector<int>> prev(n+1,vector<int>(m+1,0));
+    vector<int> lens(m+1,0); // + 1 because of line 22
+    vector<vector<int>> prev(n+1,vector<int>(m+1,0)); // +1 because of line 24 & 27
+
     int topleft;
     for(int i = 1; i <= n; i++) {
         topleft = 0;
         for(int j = 1; j <= m; j++) {
             int len;
-            if(A[i-1] == B[j-1]) {
+            if(A[i-1] == B[j-1]) { // a direct match
                 len = topleft+1;
                 prev[i][j] = 1; // topleft
             } else {
-                if(lens[j] > lens[j-1]) {
+                if(lens[j] > lens[j-1]) { //if a previous one matched
                     len = lens[j];
                     prev[i][j] = 2; // top
                 } else {
@@ -30,6 +31,12 @@ void LCS(const vector<int> &A, const vector<int> &B) {
             lens[j] = len;
         }
     }
+//    for(vector<int> v : prev){
+//        for(int item : v){
+//            cout << item << " ";
+//        }
+//        cout << endl;
+//    }
     // print LCS
     int i = n, j = m, idx = 0;
     vector<int> lcs(lens[m],0);
@@ -57,7 +64,7 @@ int main() {
     }
     for(int i = 0; i < m; i++) {
         cin >> B[i];
-    }
+    }//everything above was getting input.
     LCS(A,B);
     return 0;
 }
